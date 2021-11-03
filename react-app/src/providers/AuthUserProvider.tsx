69492d875d54a60dfa0ffdb4e60df5a2867ead37
yatto17 <ysht17@gmail.com>
2021-11-03 20:17:24 +0900

    コンテキストの修正

--- react-app/src/providers/AuthUserProvider.tsx
+++ react-app/src/providers/AuthUserProvider.tsx
@@ -3,8 +3,6 @@ import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "re
 import { User } from "types/api/user";
 
 export type AuthUserContextType = {
-  loading: boolean;
-  setLoading: Dispatch<SetStateAction<boolean>>;
   isSignedIn: boolean;
   setIsSignedIn: Dispatch<SetStateAction<boolean>>;
   currentUser: User | null;
@@ -18,15 +16,12 @@ export const AuthUserContext = createContext<AuthUserContextType>(
 export const AuthUserProvider = (props: { children: ReactNode }) => {
   const { children } = props;
 
-  const [loading, setLoading] = useState(true);
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   return (
     <AuthUserContext.Provider
       value={{
-        loading,
-        setLoading,
         isSignedIn,
         setIsSignedIn,
         currentUser,
